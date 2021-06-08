5c83788d5fa7f21b1e071b56d4382bd722a3ced2
sunny nguyen-bui <snguyenbui@gmail.com>
2021-06-07 17:31:33 -0700

    counts adjacant squares

--- june-challenge/week1/maxAreaOfIsland.js
+++ june-challenge/week1/maxAreaOfIsland.js
@@ -5,42 +5,49 @@
 const maxAreaOfIsland = function (grid) {
   let maxCount = 0;
   let count = 0;
-  for (y in grid) {
-    for (x in grid[y]) {
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
       if (grid[y][x] === 1) {
         count++;
+        if (checkUp(grid, x, y)) {
+          count++;
+        }
+        if (checkDown(grid, x, y)) {
+          count++;
+        }
       }
-    }
-    if (count > maxCount) {
-      maxCount = count;
+      if (grid[y][x] === 0) {
+        if (count > maxCount) {
+          maxCount = count;
+        }
+        count = 0;
+      }
     }
     count = 0;
   }
   return maxCount;
 };
 
-const checkLeft = function (grid, x, y) {
-  return grid[y][x - 1] === 1;
-};
-
-const checkRight = function (grid, x, y) {
-  return grid[y][x + 1] === 1;
-};
-
 const checkUp = function (grid, x, y) {
-  return grid[y - 1][x] === 1;
+  if (y !== 0) {
+    return grid[y - 1][x] === 1;
+  }
+  return false;
 };
 
 const checkDown = function (grid, x, y) {
-  return grid[y + 1][x] === 1;
+  if (y < grid.length - 1) {
+    return grid[y + 1][x] === 1;
+  }
+  return false;
 };
 
 // const grid = [[0, 0, 0, 0, 0, 0, 0, 0]];
 // const grid = [[0, 0, 1, 0, 0, 0, 0, 0]];
-const grid = [
-  [0, 0, 1, 0, 0, 0, 0, 0],
-  [0, 0, 1, 0, 0, 0, 0, 0],
-];
+// const grid = [
+//   [0, 0, 1, 0, 0, 0, 0, 0],
+//   [0, 0, 1, 0, 0, 0, 0, 0],
+// ];
 // const grid = [
 //   [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
 //   [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
@@ -48,8 +55,19 @@ const grid = [
 //   [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
 //   [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
 //   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
-//   [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
+//   [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
 //   [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
 // ];
+
+const grid = [
+  [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+  [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+  [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0],
+  [1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+];
 
 console.log(maxAreaOfIsland(grid));
